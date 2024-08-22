8ab19d173d474361b5eead4ff1a9fcba2f0c261b
Bob Owens <borbert@hotmail.com>
2024-08-22 17:05:08 -0400

    good start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,12 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { themes } from '$lib/themes';
-
-	let currentTheme = '';
-
-	onMount(() => {
-		if (typeof window !== 'undefined') {
-			const theme = window.localStorage.getItem('theme');
-			if (theme && themes.includes(theme)) {
-				document.documentElement.setAttribute('data-theme', theme);
-				currentTheme = theme;
-			}
-		}
-	});
-
-	function setTheme(event: Event) {
-		const select = event.target as HTMLSelectElement;
-		const theme = select.value;
-		if (themes.includes(theme)) {
-			const one_year = 60 * 60 * 24 * 365;
-			window.localStorage.setItem('theme', theme);
-			document.cookie = `theme=${theme}; max-age=${one_year}; path=/; SameSite=Strict;`;
-			document.documentElement.setAttribute('data-theme', theme);
-			currentTheme = theme;
-		}
-	}
-</script>
-
-<div class="mx-auto w-full pt-5">
-	<article class="prose mx-auto max-w-4xl lg:prose-xl">
-		<h3>sveltekit+tailwind+daisyui</h3>
-		<p>
-			Half of my web projects always start the same way so I figured I could save myself some time
-			and create a template.
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content text-center">
+	  <div class="max-w-md">
+		<h1 class="text-5xl font-bold">Hello there</h1>
+		<p class="py-6">
+			I am Robert (Bob) Owens, a strategic thinker and problem solver with education in pharmacy, business, computer science, data science, and intelligent systems. 
+			With extensive experience in pharmacy administration and leadership at a top non-profit health system, I leverage my analytical skills to drive change and develop innovative solutions in healthcare.
+			My passion lies in utilizing technology, analytics, and process improvement to enhance operational efficiency and improve patient outcomes, making me a valuable asset in both clinical and administrative settings.
 		</p>
-		<p>
-			I'm using <a href="https://kit.svelte.dev/">SvelteKit</a>,
-			<a href="https://tailwindcss.com/">tailwindcss</a>
-			and <a href="https://daisyui.com/">daisyui</a>.
-		</p>
-	</article>
-</div>
-
-<div class="mx-auto w-full max-w-xl px-10">
-	<select
-		data-choose-theme
-		class="select select-bordered select-primary mx-auto my-5 w-full max-w-3xl text-xl capitalize"
-		on:change={setTheme}
-	>
-		<option disabled selected>Choose a theme</option>
-		{#each themes as theme}
-			<option value={theme} class="capitalize">{theme}</option>
-		{/each}
-	</select>
-
-	<div class="card mx-auto w-96 bg-neutral text-neutral-content">
-		<div class="card-body items-center text-center">
-			<h2 class="card-title">Thing !</h2>
-			<p>Text.</p>
-			<div class="card-actions justify-end">
-				<button class="btn btn-primary">Accept</button>
-				<button class="btn btn-ghost">Deny</button>
-			</div>
-		</div>
+	  </div>
 	</div>
-</div>
+  </div>
