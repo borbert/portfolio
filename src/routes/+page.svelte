c214b2fcd3f99342efba09b7eb04973f065c7920
Bob Owens <borbert@hotmail.com>
2024-09-10 19:34:21 -0400

    trying again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -337,7 +337,7 @@ const aboutMeData = {
 	<div class="max-w-4xl mx-auto px-4">
 	  <h1 class="text-4xl font-bold mb-6 text-center">Biography</h1>
 	  <div class=" p-8 ">
-		<p class="text-text leading-relaxed">
+		<p class="leading-relaxed">
 		  With a robust background in health system pharmacy and technology, I have dedicated my career to improving healthcare systems through leadership, process improvement, data analytics, and machine learning. 
 		  Applying my formal education in healthcare, business, and computer science has afforded me a unique perspective on health care problems.  
 		  I have been involved in a myriad of projects requiring complex data analysis in areas with high degree of regulatory and financial implications for the health system.  
