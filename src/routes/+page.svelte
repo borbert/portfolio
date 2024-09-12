965b67d81e1c95acb19a45ceb20af2a03778c5ba
Bob Owens <borbert@hotmail.com>
2024-09-12 13:36:55 -0400

    reworded the military reverence in the home section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -294,7 +294,7 @@ const aboutMeData = {
 		  Hi, there.  My name is Robert (Bob) Owens. I am a strategic thinker and problem solver with education in pharmacy, business, computer science, data science, and intelligent systems. 
 		  With extensive experience in pharmacy administration and leadership at a top non-profit health system, I leverage my analytical skills to drive change and develop innovative solutions in healthcare.
 		  My passion lies in utilizing technology, analytics, and process improvement to enhance operational efficiency and improve patient outcomes, making me a valuable asset in both clinical and administrative settings.
-		  As a veteran of US military, I also bring discipline, leadership, and perserverance sharpened by service.
+		  As a veteran of US military, I also bring discipline, leadership, and perserverance to the challenges I face.
 		</p>
 	  </div>
 	</div>
